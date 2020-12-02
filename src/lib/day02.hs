40fbd81715d96b0bc5350cc9264476cdb440a7f6
Jean-Hadrien Chabran <jh@chabran.fr>
2020-12-02 17:39:41 +0100

    Add part2 for day2

--- src/lib/day02.hs
+++ src/lib/day02.hs
@@ -1,4 +1,7 @@
--- | 
+-- |
+
+-- Maybe learning about records would have helped here xD
+-- Still, it's maybe a bit incovenient but nowhere incorrect.
 
 module Day02 where
 
@@ -53,3 +56,54 @@ part1 :: IO ()
 part1 = do
   inp <- input
   print $ countValidPasswords inp
+
+-- Each policy actually describes two positions in the password, where 1 means the first character,
+-- 2 means the second character, and so on. (Be careful; Toboggan Corporate Policies have no concept
+-- of "index zero"!) Exactly one of these positions must contain the given letter.
+-- Other occurrences of the letter are irrelevant for the purposes of policy enforcement.
+parsePositions :: String -> (Integer, Integer)
+parsePositions rawPositions =
+  let raws = splitOn "-" rawPositions
+      res = map (\position -> read position :: Integer) raws
+  in (res!!0, res!!1)
+  
+parseInput2 :: String -> ((Integer, Integer), Char, String)
+parseInput2 rawInput =
+  let segments = splitOn " " rawInput
+      positions = parsePositions (head segments)
+      char = head $ segments!!1
+      password = segments!!2
+  in (positions, char, password)
+
+input2 :: IO [((Integer, Integer), Char, String)]
+input2 = do
+  raw <- readFile "data/Day02.txt"
+  let somePasswords = map parseInput2 . lines $ raw
+  return somePasswords
+  
+invalidPassword2 :: ((Integer, Integer), Char, String)
+invalidPassword2 = ((1,3), 'a', "aba")
+
+invalidPassword2' :: ((Integer, Integer), Char, String)
+invalidPassword2' = ((1,3), 'a', "bbb")
+
+validPassword2 :: ((Integer, Integer), Char, String)
+validPassword2 = ((1,3), 'a', "abb")
+
+validPassword2' :: ((Integer, Integer), Char, String)
+validPassword2' = ((1,3), 'a', "bba")
+
+isPasswordValid2 :: ((Integer, Integer), Char, String) -> Bool
+isPasswordValid2 ((p1, p2), c, str) =
+  let a = str !! (fromIntegral p1 - 1)
+      b = str !! (fromIntegral p2 - 1)
+  in (a == c && b /= c) || (a /= c && b == c)
+
+countValidPasswords2 :: [((Integer, Integer), Char, String)] -> Integer
+countValidPasswords2 entries =
+  foldl (\count entry -> if isPasswordValid2 entry then count + 1 else count) 0 entries
+
+part2 :: IO ()
+part2 = do
+  inp <- input2
+  print $ countValidPasswords2 inp
